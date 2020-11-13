2b62fdc0f5c55940a0b7e245878caf627149d1a3
Quant85 <antonio.85.q@gmail.com>
2020-11-13 17:29:51 +0100

    Auto stash before merge of "main" and "origin/main"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-// var numberOfKilometers, yourAge, yourAge, yourDiscount, fullCost, discountedCost, ticketCostGgeneretor ;
+
 
 /* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
@@ -7,6 +7,7 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65 */
 
+// var numberOfKilometers, yourAge, yourAge, yourDiscount, fullCost, discountedCost, ticketCostGgeneretor ;
 //Cosa chiedo all'utente come input
 var numberOfKilometers;
 var yourAge;
@@ -15,36 +16,53 @@ var yourAge;
 var yourDiscount;
 var fullCost;
 var discountedCost;
-var ticketCostGgeneretor;
 var costOfKilometer = 0.21;
 
-//Input Utente
-numberOfKilometers = Number(prompt("Esprimi la validità di percorrenza del titolo di viaggio in chilometri"));
-console.log(numberOfKilometers);
 
-yourAge = Number(prompt("Quanti anni hai?"));
-console.log(yourAge);
+function ticketCostGgeneretor() {
 
-//Base operetion
-fullCost = costOfKilometer * numberOfKilometers;
-console.log(fullCost);
-
-if (yourAge < 18 ) {
-
-    yourDiscount = 0.2;
-    console.log(yourDiscount);
-
-} else if (yourAge > 65) {
-
-    yourDiscount = 0.4;
-    console.log(yourDiscount);
-
-} else {
-
-    yourDiscount = 0;
-    console.log(yourDiscount);
+    //Input Utente
+    numberOfKilometers = Number(prompt("Esprimi la validità di percorrenza del titolo di viaggio in chilometri"));
+    console.log(numberOfKilometers);
+    
+    yourAge = Number(prompt("Quanti anni hai?"));
+    console.log(yourAge);
+    
+    //Base operetion
+    fullCost = costOfKilometer * numberOfKilometers;
+    console.log(fullCost);
+    
+    if (yourAge < 18 ) {
+    
+        yourDiscount = 0.2;
+        console.log(yourDiscount);
+    
+    } else if (yourAge > 65) {
+    
+        yourDiscount = 0.4;
+        console.log(yourDiscount);
+    
+    } else {
+    
+        yourDiscount = 0;
+        console.log(yourDiscount);
+    }
+    
+    //conditional operations
+    
+    //return info
+    yourTotalCost =  fullCost * ( 1 - yourDiscount);
+    console.log(yourTotalCost);
+    
+    
+    document.getElementById("total_cost").innerHTML = yourTotalCost.toFixed(2) + " \u20AC";
+    
+    document.getElementById("full_cost").innerHTML = fullCost.toFixed(2) + " \u20AC";
+    
+    document.getElementById("discount").innerHTML = yourDiscount.toFixed(2) * 100 + " \u0025";
 }
 
+<<<<<<< Updated upstream
 //conditional operations
 
 //return info
@@ -58,4 +76,6 @@ document.getElementById("full_cost").innerHTML = fullCost.toFixed(2) + " \u20AC"
 
 document.getElementById("discount").innerHTML = yourDiscount.toFixed(2) * 100 + " \u0025";
 
+=======
+>>>>>>> Stashed changes
 
